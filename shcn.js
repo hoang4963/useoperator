96170808b01bc320455f5a65d912e19bc46a3c96
hoang4963 <hoangth.finbox@gmail.com>
2022-07-11 08:45:47 +0700

    sudungtoantu

---
+++ shcn.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputheight;
+inputwidth = prompt('Nhap chieu rong');
+inputheight = prompt('nhap chieu dai');
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area = width * height;
+document.write(' S hinh chu nhat la: ' + area);
